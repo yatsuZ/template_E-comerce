434ca042d8134515479adad6ae538f0808d6b705
yatsuZ <yassine.zaoui28062003@gmail.com>
2026-01-19 17:02:23 +0100

    debut du teste a faire

---
+++ code/srcs/test/repository/user.repository.test.ts
@@ -0,0 +1,3 @@
+import {Logger} from "./../../backend/utils/logger.js"
+
+Logger.info("Je dois tester Le reposotorie de User avec la bdd")
---
+++ code/srcs/test/service/user.service.test.ts
@@ -0,0 +1,3 @@
+import {Logger} from "./../../backend/utils/logger.js"
+
+Logger.info("Je dois tester Le service de User avec la bdd et que sa marche bien quoi.")
